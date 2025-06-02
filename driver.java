a7ef593949bc277498bed52f0f28c430af118917
Alamin Adeleke <alaminadeleke5@gmail.com>
2025-06-02 15:22:04 -0300

    Update driver.java

--- driver.java
+++ driver.java
@@ -4,6 +4,6 @@ public class driver
     public static void main(int num)
     {
         for(int i = 0 ; i < num ; i++)
-            System.out.println(RandomName.maker());
+            System.out.println(RandomName.make());
     }
 }
